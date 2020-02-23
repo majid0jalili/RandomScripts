3d75b0068771b323d381fe0332942dbf18deb65f
Majid <majid0jalili@gmail.com>
2020-02-23 15:02:32 -0600

    empty cell fixed

--- gem5ExtractFolder/stat.py
+++ gem5ExtractFolder/stat.py
@@ -6,16 +6,13 @@ import csv
 def serachFileKeys(path, keys, name):
     foundVal = []
     for k in keys:
+        foundVal.append(0)
         searchfile = open(path, "r")
         for line in searchfile:
             if k in line: 
-                res = list(map(int, (re.findall(r'\b\d+\b', line)))) 
-               
-                if(res[0]>10):
-                    foundVal.append(res[0])
-                else:
-                    foundVal.append(res[1])    
-                break;
+                res = (line.split())
+                foundVal[-1] = (int(res[1]))
+                break
         searchfile.close()
     return foundVal
 
@@ -36,7 +33,7 @@ def serachFolderKeys(path, keys):
     return foundVal              
 
 def writeToCSV(result):
-    with open("./output.csv", 'a') as myfile:
+    with open("./m5out/output.csv", 'a') as myfile:
         wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
         wr.writerow(result)
 
@@ -47,8 +44,8 @@ keys=["system.switch_cpus.numCycles", "system.switch_cpus.commit.loads",
       "system.cpu.l2cache.overall_accesses::.switch_cpus.data", "system.cpu.l2cache.demand_misses::.switch_cpus.data", 
       "system.l3.overall_accesses::.switch_cpus.data", "system.l3.demand_misses::.switch_cpus.data", 
       "system.l3.DataMeta::0", "system.l3.DataMeta::1", "system.l3.DataMeta::2", "system.l3.DataMeta::3",
-      "system.l3.MetaReq_accesses::total", "system.l3.MetaReq_misses::total"
-      "system.mem_ctrls1.num_reads::total", "system.mem_ctrls1.num_writes::total",
+      "system.l3.MetaReq_accesses::total", "system.l3.MetaReq_misses::total",
+      "system.mem_ctrls1.num_reads::total", "system.mem_ctrls1.num_writes::total"
 
 
        ]
